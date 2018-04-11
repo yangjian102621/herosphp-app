a770cd31df723d7fe08986dfaccaba1e8f0f88ca
yangjian <yangjian102621@gmail.com>
2018-04-11 16:08:57 +0800

    refactor: 添加登录控制器的注释代码

--- app/modules/demo/action/LoginAction.php
+++ app/modules/demo/action/LoginAction.php
@@ -47,10 +47,18 @@ class LoginAction extends Controller
 
     }
 
+    /**
+     * 生成二维码
+     * @param HttpRequest $requests
+     */
     public function qrcode(HttpRequest $request) {
         QRcode::png("http://192.168.0.118/demo/login/check?username=xiaoming")->size(300)->show();
     }
 
+    /**
+     * 获取支付状态接口
+     * @param HttpRequest $request
+     */
     public function status(HttpRequest $request) {
 
         //超时时间
@@ -71,6 +79,10 @@ class LoginAction extends Controller
         die("TIME_OUT");
     }
 
+    /**
+     * 扫码后跳转的页面
+     * @param HttpRequest $request
+     */
     public function check(HttpRequest $request) {
         $username = $request->getParameter("username");
         //登录中
@@ -78,6 +90,10 @@ class LoginAction extends Controller
         $this->setView("login_check");
     }
 
+    /**
+     * 登录操作
+     * @param HttpRequest $request
+     */
     public function loginCheck(HttpRequest $request) {
 
         $username = $request->getParameter("username");
@@ -88,6 +104,9 @@ class LoginAction extends Controller
         die(self::LOGIN_OK);
     }
 
+    /**
+     * 登录成功跳转页面
+     */
     public function success() {
         $this->setView("login_success");
     }
