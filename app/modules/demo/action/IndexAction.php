e68f495c668fbacae40e5d024a419d94982930ea
yangjian <yangjian102621@gmail.com>
2018-04-26 19:08:53 +0800

    restruct admin module with vue framework

--- app/modules/demo/action/IndexAction.php
+++ app/modules/demo/action/IndexAction.php
@@ -1,8 +1,12 @@
 <?php
 namespace app\demo\action;
 
+use app\admin\service\AdminPermissionService;
+use app\admin\service\AdminService;
 use herosphp\core\Controller;
+use herosphp\core\Loader;
 use herosphp\http\HttpRequest;
+use herosphp\utils\JsonResult;
 
 /**
  * 首页测试
@@ -18,6 +22,19 @@ class IndexAction extends Controller {
     public function index(HttpRequest $request) {
         $this->setView("index");
         $this->assign("title", "欢迎使用Herosphp");
+    }
+
+    /**
+     * 获取权限列表
+     * @param HttpRequest $request
+     */
+    public function permissions(HttpRequest $request) {
+        $service = Loader::service(AdminPermissionService::class);
+        $items = $service->page(0, 100)->find();
+        $res = new JsonResult();
+        $res->setItems($items);
+        $res->setCode(JsonResult::CODE_SUCCESS);
+        $res->output();
     }
 
 }
