cb2e2cb98078475a32d52978903296d231004a8a
yangjian <yangjian102621@gmail.com>
2019-03-05 15:16:02 +0800

    add cross-domain request support for api application

--- app/api.php
+++ app/api.php
@@ -4,5 +4,8 @@
  * @author yangjian
  * @since v3.0.0
  */
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Methods:GET,POST');
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
 require_once __DIR__."/server.php";
 \herosphp\BootStrap::runApi(); // 启动 API 应用程序
