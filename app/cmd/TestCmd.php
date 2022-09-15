a49fc4a1999dfc4380e6c896fc6e18df9897b573
RockYang <yangjian102621@163.com>
2022-09-15 11:11:15 +0800

    update command action demo

--- app/cmd/TestCmd.php
+++ app/cmd/TestCmd.php
@@ -2,12 +2,14 @@
 
 declare(strict_types=1);
 
-namespace app\controller;
+namespace app\cmd;
 
 use herosphp\annotation\Action;
 use herosphp\annotation\Command;
 use herosphp\core\BaseCommand;
 use herosphp\core\Input;
+use herosphp\GF;
+use herosphp\utils\Logger;
 
 #[Command(TestCmd::class)]
 class TestCmd extends BaseCommand
@@ -15,6 +17,15 @@ class TestCmd extends BaseCommand
     #[Action(uri: '/cli/test')]
     public function test(Input $input)
     {
+        GF::printInfo('Run in command mode.');
+        GF::printSuccess('Actions Parameters:');
         var_dump($input->getAll());
+
+        Logger::info('Start doing something...');
+        while ($this->isRunning()) {
+            Logger::info('Task is running, press Ctrl + C to exit.');
+            sleep(1);
+        }
+        Logger::info('Task done.');
     }
 }
--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -1,18 +1,25 @@
 <?php
+
 declare(strict_types=1);
 
 namespace app\controller;
 
+use app\service\UserService;
 use herosphp\annotation\Controller;
 use herosphp\annotation\Get;
+use herosphp\annotation\Inject;
 use herosphp\core\BaseController;
 
 #[Controller(IndexController::class)]
 class IndexController extends BaseController
 {
+    #[Inject(name: UserService::class)]
+    protected UserService $userSer;
+
     #[Get(uri: '/')]
     public function index(): string
     {
-        return "hello herosphp";
+        $this->userSer->login();
+        return 'hello herosphp';
     }
 }
